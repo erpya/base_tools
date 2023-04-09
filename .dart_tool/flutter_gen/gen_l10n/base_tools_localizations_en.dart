45b256cab37cfcf918bc0cf72c91efcd5e8f5fe0
yamelsenih <ysenih@erpya.com>
2023-04-09 13:23:22 -0400

    Add translation

---
+++ .dart_tool/flutter_gen/gen_l10n/base_tools_localizations_en.dart
@@ -0,0 +1,96 @@
+import 'base_tools_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get title => 'Play';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get email => 'E-Mail';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get updateData => 'Update';
+
+  @override
+  String get automaticallyUpdate => 'Automatically Update';
+
+  @override
+  String get navigationDrawerUserName => 'User Name';
+
+  @override
+  String get navigationDrawerUserEmail => 'User e-Mail';
+
+  @override
+  String get navigationDrawerLogin => 'Login';
+
+  @override
+  String get navigationDrawerLogout => 'Logout';
+
+  @override
+  String get loading => 'Loading';
+
+  @override
+  String get fillPinCode => 'Pin Code';
+
+  @override
+  String get invalidPin => 'Invalid Pin Code';
+
+  @override
+  String get tooManyAttempts => 'Too Many Attempts';
+
+  @override
+  String get biometricAuthenticationFailed => 'Biometric Authentication Failed';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get settingPinCode => 'Setting Pin Code';
+
+  @override
+  String get changePassCode => 'Change Passcode';
+
+  @override
+  String get repeatPinCode => 'Repeat the same Pin Code';
+
+  @override
+  String get currentPin => 'Current Pin';
+
+  @override
+  String get newPin => 'New Pin';
+
+  @override
+  String get confirmNewPin => 'Confirm new Pin';
+
+  @override
+  String get useFingerprint => 'Use Fingerprint';
+
+  @override
+  String get downloading => 'Downloading';
+
+  @override
+  String get upgrading => 'Upgrading';
+
+  @override
+  String get updateFinished => 'Update Finished';
+
+  @override
+  String get noRecordsFound => 'No records found';
+
+  @override
+  String get locationDisabled => 'Location services are disabled. Please enable the services';
+
+  @override
+  String get locationDenied => 'Location permissions are denied';
+
+  @override
+  String get locationPermanentlyDenied => 'Location permissions are permanently denied, we cannot request permissions';
+}
